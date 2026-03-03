bad5ab1eebdd2bf802fcfa9bc3012e9beb3803ec
Robin Bette <robinbette@hotmail.com>
2026-03-03 14:50:31 +0100

    Use snake_case

--- tests/items/test_items.py
+++ tests/items/test_items.py
@@ -3,26 +3,26 @@ from main import app
 
 client = TestClient(app)
 
-basePath = '/items'
+base_path = '/items'
 
 def test_root():
-    response = client.get(basePath)
+    response = client.get(base_path)
     assert response.status_code == 200
 
 def test_query_item_by_id():
-    response = client.get(f"{basePath}/1")
+    response = client.get(f"{base_path}/1")
     assert response.status_code == 200
 
 def test_query_item_by_id_returns_404():
-    response = client.get(f"{basePath}/3")
+    response = client.get(f"{base_path}/3")
     assert response.status_code == 404
 
 def test_query_item_by_parameters():
-    response = client.get(f"{basePath}/?name=Nails")
+    response = client.get(f"{base_path}/?name=Nails")
     assert response.status_code == 200
 
 def test_add_item():
-    response = client.post(basePath, json={
+    response = client.post(base_path, json={
         "name": "Screwdriver",
         "price": 3.99,
         "count": 10,
